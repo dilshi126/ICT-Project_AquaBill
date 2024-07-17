5db35017b9060dc28706ae6f5051044bcb1232d9
Dilshi Wijesinghe <99238515+dilshi126@users.noreply.github.com>
2024-07-17 10:00:52 +0530

    Create manifest.js

---
+++ public/manifest.js
@@ -0,0 +1,16 @@
+
+{
+    "short_name": "Electro Care",
+    "name": "Electro Care App",
+    "icons": [
+      
+    ],
+    "start_url": "/?source=pwa",
+    "display": "standalone",
+    "scope": "/",
+    
+    "description": "",
+    "screenshots": [
+      
+    ]
+  }
